dea0698dc4460e8958054959c7b087652ad3c892
vrushal1018 <vrushalpachupate@gmail.com>
2025-07-27 23:21:51 +0530

    refactor: Remove 'Join Our Team' button from 'Join our journey' section in AboutSection

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -203,10 +203,6 @@ const AboutSection = () => {
               there's a place for you in the Fame Esports family.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
-              <Button size="lg" className="btn-gaming">
-                <Users className="w-5 h-5 mr-2" />
-                Join Our Team
-              </Button>
               <Button size="lg" variant="outline" className="btn-gaming-secondary" asChild>
                 <a href="https://forms.gle/VHYSkuvtrHbTLUdZ9" target="_blank" rel="noopener noreferrer">
                   <Star className="w-5 h-5 mr-2" />
