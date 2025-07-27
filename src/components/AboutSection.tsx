27e0df1bfee5df6c852648468a3bd85d6020077b
vrushal1018 <vrushalpachupate@gmail.com>
2025-07-27 23:21:07 +0530

    feat: Add Google Form link to 'Become a Partner' button in AboutSection

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -207,9 +207,11 @@ const AboutSection = () => {
                 <Users className="w-5 h-5 mr-2" />
                 Join Our Team
               </Button>
-              <Button size="lg" variant="outline" className="btn-gaming-secondary">
-                <Star className="w-5 h-5 mr-2" />
-                Become a Partner
+              <Button size="lg" variant="outline" className="btn-gaming-secondary" asChild>
+                <a href="https://forms.gle/VHYSkuvtrHbTLUdZ9" target="_blank" rel="noopener noreferrer">
+                  <Star className="w-5 h-5 mr-2" />
+                  Become a Partner
+                </a>
               </Button>
             </div>
           </div>
